68fab9c7cf097b696b078402bff4dd2358dbbe34
JairoMtz28 <alvaradomartinezjairojafet@gmail.com>
2024-05-21 13:49:50 -0600

    Update index.php
    Jairo

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link rel="icon"  href="icon/payKawaii.ico">
 </head>
 <body>
-    <!-- Ya aqui le mueven a la pagina principal -->
+
+    <!-- Ya aqui le mueven a la pagina principal gtft-->
     <!-- Si es posible pongan comentarios en cada cosa nueva que hagan, hanganlos sencillos, mas si es un formulario para saber
          que es lo que vamos a trabajar en BackEnd, Gracias xd  -->
 
