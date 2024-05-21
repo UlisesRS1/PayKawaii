25147384eb449436004d345c48ac1959e181e8fa
CesarE301 <cesar12345chuy11@gmail.com>
2024-05-21 14:40:19 -0600

    Create Productos.php
    se creo el archivo de la pagina correspondiente a los productos

---
+++ php/Productos.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <style>
+        .narvalP{
+            background-color: 
+        }
+
+
+
+
+    </style>
+   
+</head>
+<body>
+
+    <!-- Navbar -->
+<div class="container narvalP">
+    <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+        <img src="image/pays.jpg" class="" alt="..." height="100" width="100">
+        <a class="navbar-brand text-primary text-opacity-75" href="#">Michi reposteria el pay kawaii</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <!-- Scrollspy -->
+        <nav id="navbar-example2" class="navbar bg-body-tertiary px-3 mb-3 ">
+            <ul class="nav nav-pills align-content-center">
+              <li class="nav-item">
+                <a class="nav-link" href="#carouselExampleControls">productos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#Cards">cartas pays</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#accordion">acerca de...</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#accordion2">galeria</a>
+              </li>
+              
+            </ul>
+        </nav>
+    </nav>
+
+    <!-- Cards -->
+    <div class=" container cards background-color-brown">
+        <h1 id="Cards" class="text-center">lo que ofrecemos</h1>
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-12 col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between align-content-center p-0 bg-success text-white " style="width: 18rem;">
+                <img src="image/pay limon.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de limon</h5>
+                  <p class="card-text">Un postre clásico con una base de galleta desmenuzada, relleno de una mezcla cremosa y refrescante de jugo de limón, huevos y leche condensada, coronado con una capa de crema batida o merengue. Es ácido, dulce y refrescante, perfecto para los amantes de los cítricos.</p>
+                  
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-danger text-white" style="width: 18rem;">
+                <img src="image/pie fresa.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de fresa</h5>
+                  <p class="card-text">Delicioso postre con una base de masa crujiente, rellena de una mezcla de fresas frescas o en puré, azúcar y gelatina, y adornado con crema batida o rodajas de fresa. Es dulce, fresco y frutal, ideal para los amantes de las fresas y los postres ligeros.</p>
+                  
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-warning text-black" style="width: 18rem;">
+                <img src="image/pie.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de queso</h5>
+                  <p class="card-text"> Un clásico reconfortante con una base de galleta o masa quebrada, relleno de una mezcla suave y cremosa de queso crema, huevos, azúcar y vainilla, horneado hasta que esté dorado y ligeramente firme. Es rico, cremoso y ligeramente dulce, perfecto para aquellos que disfrutan de los postres más decadentes.</p>
+                  
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+</html>
