e9e1368512e60e9eacff7d1bd51a414e0f1701ee
UlisesRS1 <rankulimarcus@gmail.com>
2024-05-20 23:08:36 -0600

    How to work

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 </head>
 <body>
     <!-- Ya aqui le mueven a la pagina principal -->
+    <!-- Si es posible pongan comentarios en cada cosa nueva que hagan, hanganlos sencillos, mas si es un formulario para saber
+         que es lo que vamos a trabajar en BackEnd, Gracias xd -->
+    
 </body>
 </html>
