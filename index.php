e7a5c2ca3ed710caa33f86472aedd9d4173770dd
UlisesRS1 <rankulimarcus@gmail.com>
2024-05-20 18:45:28 -0600

    Agregado de icono PayKawaii en pestaña

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PayKawaii</title>
+    <link rel="icon"  href="icon/payKawaii.ico">
 </head>
 <body>
     <!-- Ya aqui le mueven a la pagina principal -->
