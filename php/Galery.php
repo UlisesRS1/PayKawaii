33d5ef43986a432e662b500f832edb98e8e3b695
UlisesRS1 <rankulimarcus@gmail.com>
2024-05-22 10:21:03 -0600

    Push

--- php/Galery.php
+++ php/Galery.php
@@ -7,7 +7,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/style.css">
-    
+
 </head>
 <body>
     <div class="container">
