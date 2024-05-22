c5d9e91cb1cdb6ad0f41e333c541ce7346810ffc
AngelN22 <101516021+AngelN22@users.noreply.github.com>
2024-05-22 09:31:19 -0600

    Angel2
    Cambio de estilos, pagina de galeria casi terminada

--- php/Galery.php
+++ php/Galery.php
@@ -15,19 +15,20 @@
             <div class="col-12 ">
                 <div class="navbar-fixed fid-sm-block d-md-none d-lg-none d-xl-none d-xxl-none">
                 <nav id="navbar-example2" class="navbar px-3 mb-3 d-flex justify-content-center align-items-center">
-                    <img src="../svg/PayKawaii.svg" alt="" width="100" height="100">
-                    <h1 class="navbar-brand text-danger fs-2" href="#">Reposteria "El pay Kawai"</h1>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="" data-bs-target="">
+                    <img src="../svg/PayKawaii.svg" alt="" width="70" height="70">
+                    <h1 class="navbar-brand text-danger fs-2" href="#">El pay Kawai</h1>
+                    <button type="button" class="btn btn1 btn-primary" data-bs-toggle="" data-bs-target="">
                                 Perfil
                     </button>
                     <ul class="nav nav-pills">
                       <li class="nav-item dropdown text-center">
-                        <a class="nav-link dropdown-toggle fs-4 morado shadow" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Contenido</a>
+                        <a class="nav-link dropdown-toggle fs-4 btn1 morado shadow" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Contenido</a>
                         <ul class="dropdown-menu">
                           <li class="nav-item"><a class="nav-link" href="#scrollspyHeading1">Inicio</a></li>
-                          <li class="nav-item"><a class="nav-link" href="#scrollspyHeading2">Ofertas</a></li>
-                          <li><a class="dropdown-item" href="#scrollspyHeading3">Conocenos</a></li>
-                          <li><a class="dropdown-item" href="#scrollspyHeading4">Galeria</a></li>
+                          <li class="nav-item"><a class="nav-link" href="#scrollspyHeading2">Nosotros</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading3">Galeria</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading4">Productos</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading4">Contacto</a></li>
                           <li><hr class="dropdown-divider"></li>
                           <li><a class="dropdown-item" href="#scrollspyHeading5">Pie de pagina</a></li>
                         </ul>
@@ -37,19 +38,20 @@
                   </div>
                   <div class="d-none d-sm-none d-md-block">
                   <nav id="navbar-example2" class="navbar px-3 mb-3">
-                    <img src="../svg/PayKawaii.svg" alt="" width="100" height="100">
-                    <h1 class="navbar-brand text-danger fs-2" href="#">Reposteria "El pay Kawai"</h1>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="" data-bs-target="">
+                    <img src="../svg/PayKawaii.svg" alt="" width="70" height="70">
+                    <h1 class="navbar-brand text-danger fs-2" href="#">El pay Kawai</h1>
+                    <button type="button" class="btn btn1 btn-primary" data-bs-toggle="" data-bs-target="">
                                 Perfil
                     </button>
                     <ul class="nav nav-pills">
                       <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle fs-4 morado shadow" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Contenido</a>
+                        <a class="nav-link dropdown-toggle fs-4 btn1 morado shadow" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Contenido</a>
                         <ul class="dropdown-menu">
-                          <li class="nav-item"><a class="nav-link" href="#scrollspyHeading1">Inicio</a></li>
-                          <li class="nav-item"><a class="nav-link" href="#scrollspyHeading2">Ofertas</a></li>
-                          <li><a class="dropdown-item" href="#scrollspyHeading3">Conocenos</a></li>
-                          <li><a class="dropdown-item" href="#scrollspyHeading4">Galeria</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#scrollspyHeading1">Inicio</a></li>
+                          <li class="nav-item"><a class="nav-link" href="#scrollspyHeading2">Nosotros</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading3">Galeria</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading4">Productos</a></li>
+                          <li><a class="dropdown-item" href="#scrollspyHeading4">Contacto</a></li>
                           <li><hr class="dropdown-divider"></li>
                           <li><a class="dropdown-item" href="#scrollspyHeading5">Pie de pagina</a></li>
                         </ul>
@@ -78,17 +80,17 @@
                 </div>
                 <div class="col-4 p-3">
                   <button type="button" class="btn"   data-bs-toggle="modal" data-bs-target="#galery-4">
-                    <img src="../img/.jpg" alt="" class="w-100">
+                    <img src="../img/flan.jpg" alt="" class="w-100">
                   </button>
                 </div>
                 <div class="col-4 p-3">
                   <button type="button" class="btn"   data-bs-toggle="modal" data-bs-target="#galery-5">
-                    <img src="img/galery-5.jpg" alt="" class="w-100">
+                    <img src="../img/flan2.jpg" alt="" class="w-100">
                   </button>
                 </div>
                 <div class="col-4 p-3">
                   <button type="button" class="btn"   data-bs-toggle="modal" data-bs-target="#galery-6">
-                    <img src="img/galery-6.jpg" alt="" class="w-100">
+                    <img src="../img/pay-de-limon.jpg" alt="" class="w-100">
                   </button>
                 </div>
             </div>
@@ -101,7 +103,7 @@
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    <img src="img/galery-1.jpg" alt="" class="w-100">
+                    <img src="../img/Pay1.jpg" alt="" class="w-100">
                   </div>
               </div>
               </div>
@@ -114,7 +116,7 @@
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                    <img src="img/galery-2.jpg" alt="" class="w-100">
+                    <img src="../img/Rebanada.jpg" alt="" class="w-100">
                   </div>
               </div>
               </div>
@@ -128,7 +130,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                  <img src="img/galery-3.jpg" alt="" class="w-100">
+                  <img src="../img/rebanada2.jpg" alt="" class="w-100">
                 </div>
             </div>
             </div>
@@ -143,7 +145,7 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                <img src="img/galery-4.jpg" alt="" class="w-100">
+                <img src="../img/flan.jpg" alt="" class="w-100">
               </div>
           </div>
           </div>
@@ -158,7 +160,7 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                <img src="img/galery-5.jpg" alt="" class="w-100">
+                <img src="../img/flan2.jpg" alt="" class="w-100">
               </div>
           </div>
           </div>
@@ -173,7 +175,7 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
-                <img src="../img/Pay1.jpg" alt="" class="w-100">
+                <img src="../img/pay-de-limon.jpg" alt="" class="w-100">
               </div>
           </div>
           </div>
