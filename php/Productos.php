014f8a6034f5732a013de39dfcd10dd30cd5027b
CesarE301 <cesar12345chuy11@gmail.com>
2024-05-22 11:13:45 -0600

    Add productos page
    Pagina de productos

--- php/Productos.php
+++ php/Productos.php
@@ -57,6 +57,53 @@
                     </ul>
                   </nav>
                   </div>
+
+                   <!-- Cards -->
+    <div class=" container cards background-color-brown">
+        <h1 id="Cards" class="text-center">lo que ofrecemos</h1>
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-12 col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between align-content-center p-0 bg-success text-white " style="width: 18rem;">
+                <img src="image/pay limon.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de limon</h5>
+                  <p class="card-text">Un postre clásico con una base de galleta desmenuzada, relleno de una mezcla cremosa y refrescante de jugo de limón, huevos y leche condensada, coronado con una capa de crema batida o merengue. Es ácido, dulce y refrescante, perfecto para los amantes de los cítricos.</p>
+                  
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-danger text-white" style="width: 18rem;">
+                <img src="image/pie fresa.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de fresa</h5>
+                  <p class="card-text">Delicioso postre con una base de masa crujiente, rellena de una mezcla de fresas frescas o en puré, azúcar y gelatina, y adornado con crema batida o rodajas de fresa. Es dulce, fresco y frutal, ideal para los amantes de las fresas y los postres ligeros.</p>
+                  
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-warning text-black" style="width: 18rem;">
+                <img src="image/pie.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de queso</h5>
+                  <p class="card-text"> Un clásico reconfortante con una base de galleta o masa quebrada, relleno de una mezcla suave y cremosa de queso crema, huevos, azúcar y vainilla, horneado hasta que esté dorado y ligeramente firme. Es rico, cremoso y ligeramente dulce, perfecto para aquellos que disfrutan de los postres más decadentes.</p>
+                </div>
+            </div>
+
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-danger text-white" style="width: 18rem;">
+                <img src="image/pie fresa.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de fresa</h5>
+                  <p class="card-text">Delicioso postre con una base de masa crujiente, rellena de una mezcla de fresas frescas o en puré, azúcar y gelatina, y adornado con crema batida o rodajas de fresa. Es dulce, fresco y frutal, ideal para los amantes de las fresas y los postres ligeros.</p>
+                  
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-12  col-lg-3 col-xl-3 col-xxl-12 card container justify-content-between p-0 bg-danger text-white" style="width: 18rem;">
+                <img src="image/pie fresa.jpeg" class="card-img-top" alt="jojos">
+                <div class="card-body">
+                  <h5 class="card-title">pay de fresa</h5>
+                  <p class="card-text">Delicioso postre con una base de masa crujiente, rellena de una mezcla de fresas frescas o en puré, azúcar y gelatina, y adornado con crema batida o rodajas de fresa. Es dulce, fresco y frutal, ideal para los amantes de las fresas y los postres ligeros.</p>
+                  
+                </div>
+            </div>
+        </div>
+    </div>
                   <div data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" class="scrollspy-example rounded-2 scrolls" tabindex="0">
 
         
